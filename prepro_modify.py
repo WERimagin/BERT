05db56c2e384ae4d4599017ceb3ea14752aa525d
笹沢裕一 <werimagin@gmail.com>
2019-05-07 00:05:42 +0900

    add

--- prepro_modify.py
+++ prepro_modify.py
@@ -117,6 +117,8 @@ def data_process(input_path,interro_path,modify_path1,modify_path2="",train=Fals
             context_text=paragraph["context"].lower()
 
             for qas in paragraph["qas"]:
+                if all_count>10:
+                    break
                 sentence_text=interro_data[all_count]["sentence_text"]
                 question_text=interro_data[all_count]["question_text"]
                 answer_text=interro_data[all_count]["answer_text"]
@@ -151,6 +153,7 @@ def data_process(input_path,interro_path,modify_path1,modify_path2="",train=Fals
                 new_qas["id"]=new_qas["id"]+"-modify_question"
                 new_qas["question"]=modify_question
                 new_paragraph["qas"].append(new_qas)
+                print(new_paragraph)
 
         new_topic["paragraphs"].append(new_paragraph)
     new_data["data"].append(new_topic)
