2cf3151c3015d32e6ec831713e5dcbea056b940e
笹沢裕一 <werimagin@gmail.com>
2019-05-07 16:55:18 +0900

    add

--- prepro_modify.py
+++ prepro_modify.py
@@ -147,7 +147,7 @@ def data_process(input_path,interro_path,modify_path1,modify_path2="",train=Fals
                 qas["modify_question"]=False
                 new_paragraph["qas"].append(qas)
 
-                if modify:
+                if modify or train==False:
                     new_qas=qas.copy()
                     new_qas["modify_question"]=True
                     new_qas["id"]=new_qas["id"]+"-modify_question"
