96112b6440f4d19d43964d47e83784a65262e46e
笹沢裕一 <werimagin@gmail.com>
2019-05-11 13:52:43 +0900

    add

--- evaluate.py
+++ evaluate.py
@@ -71,6 +71,8 @@ def evaluate(dataset, predictions):
                               ' will receive score 0.'
                     print(message, file=sys.stderr)
                     continue
+                if args.interro!="" and args.interro not in qa["question"]:
+                    continue
                 ground_truths = list(map(lambda x: x['text'], qa['answers']))
                 prediction = predictions[qa['id']]
                 exact_match += metric_max_over_ground_truths(
@@ -91,6 +93,7 @@ if __name__ == '__main__':
     parser.add_argument('--dataset_file', help='Dataset file')
     parser.add_argument('--prediction_file', help='Prediction File')
     parser.add_argument('--modify', action="store_true")
+    parser.add_argument('--interro', type=str, default="")
     args = parser.parse_args()
     with open(args.dataset_file) as dataset_file:
         dataset_json = json.load(dataset_file)
